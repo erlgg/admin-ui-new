61cc79c473fbaf2778fd51a0dd6451747710fd54
erlgg <erlanggahernawan@gmail.com>
2026-01-12 18:17:21 +0700

    atomic design page sign in dan sign up

--- src/latihan/Exercise.jsx
+++ src/latihan/Exercise.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import UserCard from "./UserCard";
 import { getUsers } from "./Services";
-
+import PostCard from "./PostCard";
 function Exercise() {
   const [users, setUsers] = useState([]);
 
@@ -19,13 +19,13 @@ function Exercise() {
 
   return (
     <>
-      <div className="min-h-screen bg-gray-100 py-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
+      <div className="min-h-screen bg-gray-100 py-48">
+        <h1 className="text-3xl font-bold text-center mb-6 text-special-red2">
+          Post Cards
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          {users.map((user) => (
-            <UserCard key={user.id} {...user} />
+          {getPosts.map((post) => (
+            <PostCard key={post.id} {...post} />
           ))}
         </div>
       </div>
